794848875830c996aa7601ec2ee6e615fe67a798
zergreen <kawhao101@gmail.com>
2023-02-04 00:58:42 +0700

    edit: add container class in root div

--- ex2/empdata.php
+++ ex2/empdata.php
@@ -1,8 +1,9 @@
 <html>
     <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
-    <div>
+    <div class="container">
         ข้อมูลพนักงานทำรายการรับซื้อเชอรี่
         <form action="emp-logic.php">
         <div id=form1>
