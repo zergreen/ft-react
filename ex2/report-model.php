b305c1079dcaba1a2ffd5dd04e590bd6dad87b45
zergreen <kawhao101@gmail.com>
2023-02-04 00:40:33 +0700

    add: v1

---
+++ ex2/report-model.php
@@ -0,0 +1,75 @@
+<div>
+    วัน xx เดือน xx ปี xxxx <br>
+    เวลา xxxx <br>
+    พนักงานรับซื้อ xxxx
+</div>
+<div>
+    <table>
+        <tr>
+            <td>รหัสสมาชิก</td>
+            <td>xxxx</td>
+        </tr>
+        <tr>
+            <td>รุ่นปลูก</td>
+            <td>xxxx</td>
+        </tr>
+        <tr>
+            <td>จำนวนครั้งที่รับซื้อเชอรี่</td>
+            <td>xxxx</td>
+        </tr>
+    </table>
+    <table class="table table-responsive table-hover table-bordered text-center">
+        <thead class="thead-dark">
+            <th>นน.เชอรี่ที่รับ (kg)</th>
+            <th>เกรดเชอรี่</th>
+            <th>นน.เชอรี่คุณภาพ (kg)</th>
+            <th>นน.เชอรี่ลอย (kg)</th>
+            <th>เงินรวม (บาท)</th>
+        </thead>
+        <tbody>
+            <td>120</td>
+            <td>A</td>
+            <td>116</td>
+            <td>A</td>
+            <td>9,300</td>
+        </tbody>
+    </table>
+    <span></span>รายการหัก
+    <div>
+        <table>
+            <tr>
+                <td>กองกลางบริหาร</td>
+                <td>120</td>
+            </tr>
+            <tr>
+                <td>เงินออมสมาชิก</td>
+                <td>60</td>
+            </tr>
+            <tr>
+                <td>เงินออมสะสมสมาชิก</td>
+                <td>560</td>
+            </tr>
+            <tr>
+                <td>รวมรายรับสุทธิ์</td>
+                <td>9,120</td>
+            </tr>
+        </table>
+    </div>
+
+    <table style="margin: 3%" width=50% class="text-center">
+        <tr>
+            <td>
+                xxxxx
+                <hr>
+                สมาชิก
+            </td>
+            <td>
+                xxxxx
+                <hr>
+                พนักงานรับซื้อ
+            </td>
+        </tr>
+    </table>
+    ___________________________________________<br>
+    <p> * ราคารับซื้อ ณ วันที่รับซื้อ A=80, B=60, C=20</p>
+</div>
--- ex2/report.php
+++ ex2/report.php
@@ -1,49 +1,21 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <div>
-        <span><h1>ต้นฉบับ</h1></span>
-        <div>
-            วัน xx เดือน xx ปี xxxx <br>
-            เวลา xxxx <br>
-            พนักงานรับซื้อ xxxx
-        </div>
-        <div>
-            <table>
-                <tr><td>รหัสสมาชิก</td> <td>xxxx</td> </tr>
-                <tr><td>รุ่นปลูก</td> <td>xxxx</td> </tr>
-                <tr><td>จำนวนครั้งที่รับซื้อเชอรี่</td> <td>xxxx</td> </tr>
-            </table>
-            <table>
-                <thead>
-                    <th>นน.เชอรี่ที่รับ (kg)</th>
-                    <th>เกรดเชอรี่</th>
-                    <th>นน.เชอรี่คุณภาพ (kg)</th>
-                    <th>นน.เชอรี่ลอย (kg)</th>
-                    <th>เงินรวม (บาท)</th>
-                </thead>
-                <tbody>
-                    <td>120</td>
-                    <td>A</td>
-                    <td>116</td>
-                    <td>A</td>
-                    <td>9,300</td>
-                </tbody>
-            </table>
-            <span></span>รายการหัก
-            <div>
-                <table>
-                    <tr><td>กองกลางบริหาร</td> <td>120</td></tr>
-                    <tr><td>เงินออมสมาชิก</td> <td>60</td></tr>
-                    <tr><td>เงินออมสะสมสมาชิก</td> <td>560</td></tr>
-                    <tr><td>รวมรายรับสุทธิ์</td> <td>9,120</td></tr>
-                </table>
-            </div>
-           
-        </div>
-        <hr>
-        <span><h1>ต้นฉบับ</h1></span>
 
-    </div>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<div class="container">
+    <span>
+        <h1>ต้นฉบับ</h1>
+    </span>
+    <?php include "report-model.php" ?>
+
+    <hr style="border: 1px dashed;">
+
+    <span>
+        <h1>สำเนา</h1>
+    </span>
+    <?php include "report-model.php" ?>
+</div>
+
 </html>
