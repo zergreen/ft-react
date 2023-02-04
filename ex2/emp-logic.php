3ae2f81b4cbf777f5fade496744847435fc47622
zergreen <kawhao101@gmail.com>
2023-02-04 07:02:01 +0700

    employee

---
+++ ex2/emp-logic.php
@@ -0,0 +1,13 @@
+<?php
+    include "../conn/conn.php";
+
+    $name = $_POST['emp_name'];
+    $surname = $_POST['emp_surname'];
+    $sex = $_POST['emp_sex'];
+    $date = $_POST['emp_birthdate'];
+    $address = $_POST['emp_address'];
+    $tel = $_POST['emp_tel'];
+    $line = $_POST['emp_line'];
+    $facebook = $_POST['emp_facebook'];
+    $email = $_POST['emp_email'];
+?>
--- ex2/empdata.php
+++ ex2/empdata.php
@@ -4,30 +4,30 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <div class="container">
-        ข้อมูลพนักงานทำรายการรับซื้อเชอรี่
-        <form action="emp-logic.php">
+        ข้อมูลพนักงานทำรายการรับซื้อเชอรี่สามโคก
+        <form action="emp-logic.php" method="post">
         <div id=form1>
         <table width='100%'>
             <tr>
                 <td>ชื่อ</td>
-                <td><input type="text" name="emp_name"></td>
+                <td><input type="text" name="emp_name" value="พงศกร"></td>
                 <td>นามสกุล</td>
-                <td><input type="text" name="emp_surname"></td>
+                <td><input type="text" name="emp_surname" value="กาเหว่าลาย"></td>
             </tr>
             <tr>
                 <td>เพศ</td>
-                <td><input type="text" name="emp_sex"></td>
+                <td><input type="text" name="emp_sex" value="ชาย"></td>
                 <td>วันเดือนปีเกิด</td>
-                <td><input type="datetime-local"></td>
+                <td><input type="datetime-local" name="emp_birthdate" value="2002-01-14T08:00"></td>
             </tr>
             <tr>
                 <td>ที่อยู่</td>
-                <td><input type="text" name="emp_address"></td>
+                <td><input type="text" name="emp_address" value="55/2 ต.ลานตากฟ้า อ.นครชัยศรี จ.นครปฐม 73120"></td>
             </tr>
-            <tr><td>เบอร์โทร</td><td><input type="text" name="emp_tel"></td></tr>
-            <tr><td>Line</td><td><input type="text" name="emp_line"></td></tr>
-            <tr><td>Facebook</td><td><input type="text" name="emp_facebook"></td></tr>
-            <tr><td>Email</td><td><input type="text" name="emp_email"></td></tr>
+            <tr><td>เบอร์โทร</td><td><input type="text" name="emp_tel" value="089-795-6072"></td></tr>
+            <tr><td>Line</td><td><input type="text" name="emp_line" value="grenkukuku"></td></tr>
+            <tr><td>Facebook</td><td><input type="text" name="emp_facebook" value="พงศกร กาเหว่าลาย"></td></tr>
+            <tr><td>Email</td><td><input type="text" name="emp_email" value="63050159@kmitl.ac.th"></td></tr>
 
         </table>
         </div>
